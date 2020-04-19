3f14ec358f47d0dd0fcee3cef144abc98934f48a
Vihar <vihardesai1833@outlook.com>
2020-04-19 16:30:47 -0400

    First Commit

---
+++ src/config/config.ts
@@ -0,0 +1,25 @@
+require('custom-env').env('dev')
+console.log(process.env.POSTGRESS_USERNAME)
+console.log(process.env.POSTGRESS_HOST)
+export const config = {
+  "dev": {
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_BUCKET
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+  "jwt": {
+    "secret": "helloworld"
+  }
+}
